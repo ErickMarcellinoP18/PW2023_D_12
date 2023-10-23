80805b73ffb4d7e1a5609fed1c19c51d5bb5209d
RockySito <andilolorocky@gmail.com>
2023-10-23 21:04:04 +0700

    login dan register view done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,127 +9,131 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
     <!-- Custom CSS -->
     <style>
-    /* Navbar Style */
-    @font-face {
-        font-family: montserrat;
-        src: url('fonts/Montserrat-VariableFont_wght.ttf');
-    }
-    * {
-        font-family: montserrat;
-    }
-    .navbar {
-        background-color: transparent;
-    }
+        /* Navbar Style */
+        @font-face {
+            font-family: montserrat;
+            src: url('fonts/Montserrat-VariableFont_wght.ttf');
+        }
 
-    h2{
-        color: white;
-    }
+        * {
+            font-family: montserrat;
+        }
 
-   
-    .navbar-nav .nav-item .nav-link {
-        font-size: 1.2rem; /* Adjust the font size as needed */
-        text-shadow: 2px 2px black; /* Adjust the shadow as needed */
-        font-weight: bold;
-        color: white !important; /* Set the text color to black with !important */
-    }
+        .navbar {
+            background-color: transparent;
+        }
+
+        h2 {
+            color: white;
+        }
 
 
-    .navbar-nav .nav-link:hover {
-        font-size: 1.3rem;
-    }
-    
+        .navbar-nav .nav-item .nav-link {
+            font-size: 1.2rem;
+            /* Adjust the font size as needed */
+            text-shadow: 2px 2px black;
+            /* Adjust the shadow as needed */
+            font-weight: bold;
+            color: white !important;
+            /* Set the text color to black with !important */
+        }
 
-    .banner-image {
-        background-image: url('images/home1.png');
-        background-size: cover;
-    }
-    
 
-    .navbar-brand img {
-        max-width: 100px;
-    }
-    
-    .grid-container {
-    display: grid;
-    grid-template-columns: 1fr 1fr; /* Membagi kontainer menjadi dua kolom dengan lebar yang sama */
-    gap: 20px; /* Jarak antara kotak kiri dan kanan */
-    margin: 20px; /* Margin umum di sekitar kontainer */
-}
+        .navbar-nav .nav-link:hover {
+            font-size: 1.3rem;
+        }
 
-.grid-child {
-    padding: 20px; /* Isi di dalam setiap kotak */
-    background-color: #f0f0f0; /* Warna latar belakang untuk kotak-kotak */
-    border: 1px solid #ddd; /* Garis pembatas di sekitar kotak-kotak */
-}
 
-.image {
-    /* Kontainer gambar */
-    text-align: center; /* Untuk mengatur gambar ke tengah */
-}
+        .banner-image {
+            background-image: url('images/home1.png');
+            background-size: cover;
+        }
 
-@media (max-width: 768px) {
-    .grid-container {
-        grid-template-columns: 1fr; /* Untuk tampilan seluler, hanya satu kolom */
-    }
-}
 
-.card{
-    margin: 50px;
-    display: flex;
-}
+        .navbar-brand img {
+            max-width: 100px;
+        }
 
-.card-text-big{
-    font-size: 20px;
-}
+        .grid-container {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            /* Membagi kontainer menjadi dua kolom dengan lebar yang sama */
+            gap: 20px;
+            /* Jarak antara kotak kiri dan kanan */
+            margin: 20px;
+            /* Margin umum di sekitar kontainer */
+        }
 
-    
-</style>
+        .grid-child {
+            padding: 20px;
+            /* Isi di dalam setiap kotak */
+            background-color: #f0f0f0;
+            /* Warna latar belakang untuk kotak-kotak */
+            border: 1px solid #ddd;
+            /* Garis pembatas di sekitar kotak-kotak */
+        }
+
+        .image {
+            /* Kontainer gambar */
+            text-align: center;
+            /* Untuk mengatur gambar ke tengah */
+        }
+
+        @media (max-width: 768px) {
+            .grid-container {
+                grid-template-columns: 1fr;
+                /* Untuk tampilan seluler, hanya satu kolom */
+            }
+        }
+
+        .card {
+            margin: 50px;
+            display: flex;
+        }
+
+        .card-text-big {
+            font-size: 20px;
+        }
+    </style>
 
 
 </head>
-<body>
-            <!-- Navbar  -->
-    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
-      <div class="container">
-        <a class="navbar-brand" href="#home">
-            <img src="{{asset('images\logoWhite.png')}}" alt="logo">
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <div class="mx-auto"></div>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="#about">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#venue">Venue</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#package">Package</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#testimony">Testimony</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#contact">Contact Us</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#signin">Sign In</a>
-            </li>
-          </ul>
+<body>
+    <!-- Navbar  -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3">
+        <div class="container">
+            <a class="navbar-brand" href="#home">
+                <img src="{{asset('images\logoWhite.png')}}" alt="logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <div class="mx-auto"></div>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#venue">Venue</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#package">Package</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimony">Testimony</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#contact">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Sign In</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
     </nav>
 
     <!-- Banner Image  -->
@@ -141,23 +146,24 @@
     </div>
     <div class="row">
         <div class="col-sm-6">
-                <div class="card" style="width: 40rem;">
-                    <img class="card-img-top" src="{{ asset('images/lampu.jpeg') }}" alt="Card image cap">
-                </div>
+            <div class="card" style="width: 40rem;">
+                <img class="card-img-top" src="{{ asset('images/lampu.jpeg') }}" alt="Card image cap">
+            </div>
         </div>
         <div class="col-sm-6">
             <div class="card" style="width: 40rem;">
                 <div class="card-body">
                     <h5 class="card-title">Welcome to Say Yes</h5>
-                    <p class="card-text-big">The perfect solution for your wedding needs! We are professional wedding event organizer, 
-                        ready to provide an exceptional experience in planning and executing various types of wedding dreams.With our 
-                        experienced team, trusted vendor network, and our commitment to client satisfaction, we have built a reputation 
+                    <p class="card-text-big">The perfect solution for your wedding needs! We are professional wedding event organizer,
+                        ready to provide an exceptional experience in planning and executing various types of wedding dreams.With our
+                        experienced team, trusted vendor network, and our commitment to client satisfaction, we have built a reputation
                         as the top choice in the industry. Trust us to turn your wedding vision into an unforgettable reality.</p>
                 </div>
             </div>
-        </div>  
+        </div>
     </div>
-<!-- Bootstrap JS (Popper.js and Bootstrap JS) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.min.js"></script>
+    <!-- Bootstrap JS (Popper.js and Bootstrap JS) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.min.js"></script>
 </body>
+
 </html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+
+<body class="antialiased">
+    <style>
+        body {
+            font-family: Roboto, Helvetica, Arial, sans-serif;
+        }
+
+        section {
+            width: 100%;
+            min-height: 100vh;
+            justify-content: center;
+            align-items: center;
+            display: flex;
+        }
+
+        .background-radial-gradient {
+            background-image: url('images/home1.png');
+            background-size: cover;
+            height: 400px;
+        }
+
+        .bg-glass {
+            background-color: #E5BDA7 !important;
+            backdrop-filter: saturate(200%) blur(25px);
+            border-color: #74583E;
+            border-width: 4px;
+            width: 550px;
+            height: 650px;
+            border-radius: 50px;
+        }
+
+        .card {
+            margin: 0 auto;
+            float: none;
+        }
+
+        .logo {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .btn-link {
+            color: #74583E;
+        }
+
+        .btn-primary {
+            background-color: #74583E;
+            border-color: #74583E;
+            margin: 0 auto;
+            display: flex;
+            align-items: center;
+        }
+
+        .form-label {
+            font-family: 'Jomolhari';
+            font-size: 20px;
+            color: white;
+        }
+    </style>
+    <header>
+        <nav class="navbar navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand">
+                    <img style="width: 50px; height: 50px; border-radius: 370px" src="{{asset('images\logo.jpg')}}" alt="logo" />
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+
+            <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+                <div class="bg-dark px-4">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/')}}">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('profil')}}">Venue</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('form')}}">Package</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('form')}}">Testimony</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <section class="background-radial-gradient overflow-hidden">
+        <div class="col-lg-6 mb-5 mb-lg-0 position-center">
+            <div class="card bg-glass">
+                <div class="card-body px-5 py-5 px-md-5">
+                    <div class="logo">
+                        <img style="width: 50%; height: 50%; border-radius: 129.50px; border: 2px #74583E solid" src="{{asset('images\logoLogin.jpg')}}" alt="logo login" />
+                    </div>
+                    <form class="form" style="margin-top: 35px;" action="{{ url('gyms') }}">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="usernameInput" class="form-label">Username</label>
+                            <input type="username" class="form-control" id="usernameInput" required />
+                        </div>
+                        <div class="mb-3">
+                            <label for="passInput" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="passInput" required />
+                        </div>
+                        <button type="button" class="btn btn-link"><a class="nav-link" href="{{ url('/')}}">Forgot Password?</a></button>
+                        <button type="button" class="btn btn-link" style="margin-left: 200px;"><a class="nav-link" href="{{ url('/register')}}">Register</a></button>
+
+                        <!-- Submit button -->
+                        <button type="submit" style="width: auto; border-radius: 15px;" class="btn btn-primary btn-block mb-2 mt-3 ">
+                            LOGIN
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
