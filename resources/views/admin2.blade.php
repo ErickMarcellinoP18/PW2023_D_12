65916a1374aeb1fa820658c34fcad854fdf864c6
Enriq18 <Enriqtrisfan01@gmail.com>
2023-12-15 17:58:45 +0700

    UI admin, karyawan n vendor

--- resources/views/admin2.blade.php
+++ resources/views/admin2.blade.php
@@ -1,4 +1,6 @@
-<!DOCTYPE html>
+<!DOCTYPE html> <!-- Main content -->
+
+<html lang="en">
 <html lang="en">
 
 <head>
@@ -49,8 +51,6 @@
     .card {
         margin: 0 auto;
         float: none;
-        background-color: #2C2C2C;
-        color: white;
     }
 
     .logo {
@@ -64,7 +64,7 @@
     }
 
     .btn-primary {
-        background-color: green;
+        background-color: purple;
         border-color: #74583E;
         margin: 0 auto;
         display: flex;
@@ -72,33 +72,35 @@
     }
 
     table {
-        width: 80%;
-        margin-left: 40px;
-        margin-bottom: 20px;
+        width: 100%;
     }
 </style>
+
 
 <header>
     <nav class="navbar navbar-dark bg-dark">
         <div class="container-fluid">
-            <a href="{{asset('images\logoWhite.png')}}" class="navbar-brand">
+            <a class="navbar-brand">
                 <img style="width: 5rem" src="{{asset('images\logoWhite.png')}}" alt="logo" />
             </a>
-            <h1 style="color: white; margin-left: -40px; margin-top: 10px">Admin Page</h1>
+            <h1><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M12 14v8H4a8 8 0 0 1 8-8Zm0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6s6 2.685 6 6s-2.685 6-6 6Zm9 4h1v5h-8v-5h1v-1a3 3 0 1 1 6 0v1Zm-2 0v-1a1 1 0 1 0-2 0v1h2Z" />
+                </svg> Admin Page
+            </h1>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                <span class="navbar-toggler-icon"></span> </button>
         </div>
-
 
         <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
             <div class="bg-dark px-4">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                     <li class="nav-item">
                         <a class="nav-link" href="{{('admin')}}">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                <path fill="currentColor" d="M12 14v8H4a8 8 0 0 1 8-8Zm0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6s6 2.685 6 6s-2.685 6-6 6Zm9 4h1v5h-8v-5h1v-1a3 3 0 1 1 6 0v1Zm-2 0v-1a1 1 0 1 0-2 0v1h2Z" />
-                            </svg> <strong>Admin Page</strong>
+                            <strong>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                    <path fill="currentColor" d="M12 14v8H4a8 8 0 0 1 8-8Zm0-1c-3.315 0-6-2.685-6-6s2.685-6 6-6s6 2.685 6 6s-2.685 6-6 6Zm9 4h1v5h-8v-5h1v-1a3 3 0 1 1 6 0v1Zm-2 0v-1a1 1 0 1 0-2 0v1h2Z" />
+                                </svg> Admin Page
+                            </strong>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -109,7 +111,7 @@
                                     <path d="M10.78 11.75h.48v-.43a6.7 6.7 0 0 1 3.75-6a4.62 4.62 0 1 0-4.21 6.46Zm13.98-.47v.43h.48A4.58 4.58 0 1 0 21 5.29a6.7 6.7 0 0 1 3.76 5.99Zm-2.47 5.17a21.45 21.45 0 0 1 5.71 2a2.71 2.71 0 0 1 .68.53H34v-3.42a.72.72 0 0 0-.38-.64a18 18 0 0 0-8.4-2.05h-.66a6.66 6.66 0 0 1-2.27 3.58ZM6.53 20.92A2.76 2.76 0 0 1 8 18.47a21.45 21.45 0 0 1 5.71-2a6.66 6.66 0 0 1-2.27-3.55h-.66a18 18 0 0 0-8.4 2.05a.72.72 0 0 0-.38.64V22h4.53Zm14.93 5.77h5.96v1.4h-5.96z" />
                                     <path d="M32.81 21.26h-6.87v-1a1 1 0 0 0-2 0v1H22v-2.83a20.17 20.17 0 0 0-4-.43a19.27 19.27 0 0 0-9.06 2.22a.76.76 0 0 0-.41.68v5.61h7.11v6.09a1 1 0 0 0 1 1h16.17a1 1 0 0 0 1-1V22.26a1 1 0 0 0-1-1Zm-1 10.36H17.64v-8.36h6.3v.91a1 1 0 0 0 2 0v-.91h5.87Z" />
                                 </g>
-                            </svg>Daftar Karyawan
+                            </svg> Daftar Karyawan
                         </a>
                     </li>
                     <li class="nav-item">
@@ -134,12 +136,12 @@
 
 <body class="antialiased">
     <div class="col-3" style="margin:auto; margin-top:60px;">
-        <div class="card" style="background-color: dark; color:white;">
-            <img style="margin:auto; margin-top:30px; padding:5px; height: 300px; width:300px; border-radius:100%" src="{{$dude['gambar']}}">
+        <div class="card" style="background-color: #74583E; color:white;">
+            <img style="margin:auto; padding:5px; height: 300px; width:300px; border-radius:100%" src="{{$dude['gambar']}}">
             <div class="card-body">
                 <table>
                     <tr>
-                        <th colspan="2" style="text-align: center; font-size:20px">Profile Admin {{ $dude['nickname'] }}</th>
+                        <th colspan="2" style="text-align: center;">Profile Admin {{ $dude['nickname'] }}</th>
                     </tr>
                     <tr>
                         <td>&nbsp;</td>
@@ -157,19 +159,25 @@
                         <td>{{ $dude['ruang'] }}</td>
                     </tr>
                 </table>
+
+
             </div>
         </div>
-        <div class="float-right col-3" style="margin-left:auto; text-align:right" ;>
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                <button type="submit" style="width: 125px; height: 50px; border-radius: 8px; margin-left:-160px" class="btn btn-primary btn-block mb-3 mt-3">
-                    <a style="font-size:20px;" class="nav-link" href="{{('editAdmin')}}">
-                        <strong>Edit Data</strong></a>
-                </button>
-            </ul>
-        </div>
-        <br>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-        </script>
+    </div>
+    <div class="float-right col-3" style="margin-left:auto; text-align:right" ;>
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+            <button type="submit" style="width: auto; border-radius: 15px;" class="btn btn-primary btn-block mb-2 mt-3">
+                <a style="font-size:13px;" class="nav-link" href="{{('editAdmin')}}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+                            <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1" />
+                            <path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3l8.385-8.415zM16 5l3 3" />
+                        </g>
+                    </svg><strong>Edit Data</strong></a>
+            </button>
+    </div>
+    <br>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
