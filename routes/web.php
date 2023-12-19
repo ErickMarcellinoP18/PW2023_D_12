b246f51ae93050df0a8c949ef8239f9c95659f06
Enriq18 <Enriqtrisfan01@gmail.com>
2023-12-19 16:16:53 +0700

    perbaiki web.php

--- routes/web.php
+++ routes/web.php
@@ -45,88 +45,11 @@ Route::get('/register', function () {
 });
 
 Route::get('/admin', function () {
-    return view('admin', [
-        'dude' => [
-            'gambar' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwODZ-msfh5mYs7LmWCY3tWtF8vmH-RMwSOC8KM_am2w&s.jpg",
-            'nLengkap' => 'Admin Ganteng Sejagat',
-            'nickname' => 'Bapak Admin',
-            'jabatan' => 'Manager',
-            'ruang' => '3315'
-        ]
-    ]);
-});
-
-
-Route::get('/editAdmin', function () {
-    return view('editAdmin', [
-        'dude' => [
-            'gambar' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwODZ-msfh5mYs7LmWCY3tWtF8vmH-RMwSOC8KM_am2w&s.jpg",
-            'nLengkap' => 'John Everest',
-            'nickname' => 'John',
-            'jabatan' => 'Senior Manager',
-            'ruang' => '3315',
-            'password' => 'bekerjaYuk1!'
-        ]
-    ]);
-});
-
-Route::get('/editKaryawan', function () {
-    return view('editKaryawan');
+    return view('admin');
 });
 
 Route::get('/vendor', function () {
-    return view('vendor',  [
-        'vendor' => [
-            [
-                'nama' => 'Club Kelapa Gading',
-                'gambar' => 'images\logo vendor2.jpg',
-            ],
-            [
-                'nama' => 'Conrad Hotels & Resorts',
-                'gambar' => 'images\logo vendor1.jpg',
-            ],
-            [
-                'nama' => 'JHL Collections',
-                'gambar' => 'images\kogo vendor2.jpg',
-            ],
-            [
-                'nama' => 'MDC Hall',
-                'gambar' => 'https://images.weddingku.com/images/upload/partners/pp/38532-m52tkx7jje4g.jpeg',
-            ],
-            [
-                'nama' => 'Raffles Hotels & Resorts',
-                'gambar' => 'images\logo2.jpg',
-            ],
-            [
-                'nama' => 'Ayana Midplaza Jakarta',
-                'gambar' => 'images\logo3.jpg',
-            ],
-            [
-                'nama' => 'Silver Dust Decoration',
-                'gambar' => 'images\logo4.jpg',
-            ],
-            [
-                'nama' => 'Kana Entertainment',
-                'gambar' => 'images\logo5.jpg'
-            ]
-        ]
-    ]);
-});
-
-Route::get('/admin', function () {
-    return view('admin', [
-        'dude' => [
-            'gambar' => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwODZ-msfh5mYs7LmWCY3tWtF8vmH-RMwSOC8KM_am2w&s.jpg",
-            'nLengkap' => 'John Everest',
-            'nickname' => 'John',
-            'jabatan' => 'Senior Manager',
-            'ruang' => '3315'
-        ]
-    ]);
-});
-
-Route::get('/tambahPegawai', function () {
-    return view('tambahPegawai');
+    return view('vendor');
 });
 
 // Login route
